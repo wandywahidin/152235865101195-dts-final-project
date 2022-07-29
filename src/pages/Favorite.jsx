9c6756b51f8a251ef22fc96c919921ce6d722ed7
wandy <wandywahidin53@gmail.com>
2022-07-29 14:20:16 +0700

    revisi

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -3,12 +3,12 @@ import FavoritePokemon from "../components/FavoritePokemon";
 
 const Favorite = ({ handleToDetail }) => {
   return (
-    <>
+    <div className="h-screen">
     <div className="text-center font-bold text-2xl mt-2">My Favorite Pokemon</div>
       <div className=" grid grid-cols-2 md:grid-cols-5 mx-2 px-1">
         <FavoritePokemon handleToDetail={handleToDetail} />
       </div>
-    </>
+    </div>
   );
 };
 
